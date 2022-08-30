738b3ccbdad9302cf86ba7ebc107f6dc02bfff32
qxz1cv4 <felipe.antonio.maria@gmail.com>
2022-08-30 21:58:57 +0100

    Corrected and expanded test cases for Quicksort. Refactored to improve legibility. Add some comments and javadoc where was applicable.

--- src/main/java/br/com/fantonio/datastructures/quicksort/Quicksort.java
+++ src/main/java/br/com/fantonio/datastructures/quicksort/Quicksort.java
@@ -3,33 +3,46 @@ package br.com.fantonio.datastructures.quicksort;
 public class Quicksort {
 
     public void sort(Integer[] array) {
+
         this.quickSort(array, 0, array.length - 1);
     }
 
-    private void quickSort(Integer[] array, int iLeft, int iRight) {
+    private void quickSort(Integer[] array, int leftBound, int rightBound) {
 
-        if (iRight - iLeft <= 0) {
+        // Base condition. Lists with size 0 or 1 because it's already sorted.
+        if (rightBound - leftBound <= 0) {
             return;
         } else {
-            int pivot = array[iRight];
-            int partition = partitionFunction(array, iLeft, iRight, pivot);
-            quickSort(array, iLeft, partition - 1);
-            quickSort(array, partition+1, iRight);
+            int pivot = array[rightBound];
+            int partition = partitionFunction(array, leftBound, rightBound, pivot);
+            quickSort(array, leftBound, partition - 1);
+            quickSort(array, partition+1, rightBound);
         }
 
     }
 
-
-    private int partitionFunction(Integer[] array, int iLeft, int iRight, int pivot) {
-
-        int leftPointer = iLeft;
-        int rightPointer = iRight - 1;
+    /**
+     * Sort a pivot inside a partition defined by the bound
+     * 
+     * @param array
+     * @param leftBound
+     * @param rightBound
+     * @param pivot
+     * @return
+     */
+    private int partitionFunction(Integer[] array, int leftBound, int rightBound, int pivot) {
+        
+        int leftPointer = leftBound;
+        int rightPointer = rightBound - 1; // to avoid checking the pivot value
 
         while (true) {
+
+            // Check for a value greater than pivot to swap it
             while (array[leftPointer] < pivot) {
                 leftPointer++;
             }
 
+            // Check for a value minor than pivot to swap it
             while (rightPointer > 0 && array[rightPointer] > pivot) {
                 rightPointer--;
             }
@@ -38,23 +51,29 @@ public class Quicksort {
             if (leftPointer >= rightPointer) {
                 break;
             } else {
-
-                Integer leftValue = array[leftPointer];
-                Integer rightValue = array[rightPointer];
-
-                array[leftPointer] = rightValue;
-                array[rightPointer] = leftValue;
+                // Swap positions between left and right poitner
+                swap(array, leftPointer, rightPointer);
             }
         }
 
-        Integer leftValue = array[leftPointer];
-        Integer rightValue = array[iRight];
-
-        array[leftPointer] = rightValue;
-        array[iRight] = leftValue;
+        // swap the pivot
+        swap(array, leftPointer, rightBound);
 
         return leftPointer;
     }
 
+    /**
+     * Swap positions in the array due to the left and right pointers
+     * @param array
+     * @param leftPointer
+     * @param rightPointer
+     */
+    private void swap(Integer[] array, int leftPointer, int rightPointer) {
+        Integer leftValue = array[leftPointer];
+        Integer rightValue = array[rightPointer];
+
+        array[leftPointer] = rightValue;
+        array[rightPointer] = leftValue;
+    }
 
 }
--- src/test/java/br/com/fantonio/datastructures/quicksort/QuicksortTest.java
+++ src/test/java/br/com/fantonio/datastructures/quicksort/QuicksortTest.java
@@ -7,6 +7,37 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class QuicksortTest {
 
+
+    @Test
+    public void testWith1() {
+        Integer[] array = {2};
+
+        Quicksort qs = new Quicksort();
+        qs.sort(array);
+
+        assertThat(array, Matchers.arrayContaining(new Integer[] {2}));
+    }
+
+    @Test
+    public void testWith2() {
+        Integer[] array = {2,0};
+
+        Quicksort qs = new Quicksort();
+        qs.sort(array);
+
+        assertThat(array, Matchers.arrayContaining(new Integer[] {0,2}));
+    }
+
+    @Test
+    public void testWith3() {
+        Integer[] array = {2,1,3};
+
+        Quicksort qs = new Quicksort();
+        qs.sort(array);
+
+        assertThat(array, Matchers.arrayContaining(new Integer[] {1,2,3}));
+    }
+
     @Test
     public void testWith5() {
         Integer[] array = {2, 4, 5, 3, 1};
@@ -24,7 +55,7 @@ public class QuicksortTest {
         Quicksort qs = new Quicksort();
         qs.sort(array);
 
-        assertThat(array, Matchers.arrayContaining(new Integer[] {1, 2, 3, 4, 5, 6 ,7, 8, 9}));
+        assertThat(array, Matchers.arrayContaining(new Integer[] {0, 1, 2, 3, 4, 5, 6 ,7, 8, 9}));
     }
 
 
