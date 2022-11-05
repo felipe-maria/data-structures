51946cb6c375a00a64b3ecbde12a9da96585ba2d
qxz1cv4 <felipe.antonio.maria@gmail.com>
2022-11-05 12:45:27 +0000

    Incluido equals e Hashcode na classe Carro

--- src/main/java/br/com/fantonio/datastructures/cs14/map/Carro.java
+++ src/main/java/br/com/fantonio/datastructures/cs14/map/Carro.java
@@ -1,5 +1,7 @@
 package br.com.fantonio.datastructures.cs14.map;
 
+import java.util.Objects;
+
 public class Carro {
 
     private String nome;
@@ -17,4 +19,16 @@ public class Carro {
         return "Carro: " + this.nome;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Carro carro = (Carro) o;
+        return Objects.equals(nome, carro.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome);
+    }
 }
